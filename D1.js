f2bdece92552986ae8828996a42ac3df820b6583
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-05-06 15:38:44 +0200

    Fino ad esercizio 5
    Non ho ben capito la richiesta dell'esercizio 6

---
+++ D1.js
@@ -0,0 +1,70 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* 1) Number: Rappresenta i dati numerici in tutte le loro forme, sia interi che decimali.
+   2) String: Rappresente i dati con una sequenza di caratteri. Deve essere delimitato tra apici singoli '' o doppi ""
+   3) Boolean: Rappresenta un valore di verità, binario, che puà essere solo o "true" o "false" (vero o falso)
+   4) Null: Rappresenta l'assenza di valore intenzionale. Quuindi è un tipo di dato che avrà sempre il valore "null"
+   5) Undenfined: Rappresenta una variabile alla quale non è stato assegnato alcun valore.
+   6) Object: Una o piu coppie Nome/valore, dove il valore può essere qualsiasi tipo di dato.
+   7) Array: Rappresenta una lista ordinata di elementi, dove ogni elemento può essere qualsiasi tipo di dato.
+   8) Symbol: Tipo di dato immutabile, può essere utilizzato come identificatore unico.
+   9) Function: una funzione è un insieme di istruzioni racchiuse in un blocco di codice, che può essere contraddistinto da un nome,
+    può accettare argomenti o parametri di ingresso e restituire dei valori.
+
+   */
+
+/* ESERCIZIO 2
+ Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+
+const myName = "Umberto"
+console.log ("il mio nome è: ", myName)
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+const numero1 = 12;
+const numero2 = 20;
+const somma = numero1 + numero2;
+console.log("La somma di", numero1, "e", numero2, "è:", somma);
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+const prova = 12;
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+
+const myName = "Amoroso"
+console.log("il mio cognome è:", myName)
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
